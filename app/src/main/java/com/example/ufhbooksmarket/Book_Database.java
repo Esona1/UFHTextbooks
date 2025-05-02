b0efc05999d46bb4d4df5183a3b2c0896bb32dc7
Esona1 <223031641@ufh.ac.za>
2025-05-02 21:38:34 +0200

    Update Book_Database.java

--- app/src/main/java/com/example/ufhbooksmarket/Book_Database.java
+++ app/src/main/java/com/example/ufhbooksmarket/Book_Database.java
@@ -9,18 +9,7 @@ public class Book_Database {
         books = new ArrayList<>();
     }
 
-    public static Book_Database getInstance() {
-        if (instance == null) {
-            instance = new Book_Database();
-        }
-        return instance;
-    }
-
-    public boolean addBook(Book book) {
-        for (Book b : books) {
-            if (b.getTitle().equalsIgnoreCase(book.getTitle()) && b.getSeller().equalsIgnoreCase(book.getSeller())) {
-                return false;
-            }
+  
         }
         books.add(book);
         return true;
